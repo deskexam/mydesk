52e7f2fc23e1db729e12cbfe71d3b64df0affa93
Vaibhav <vaibhavselkar@example.com>
2026-03-28 15:10:19 +0530

    ok not listening error

--- backend/main.py
+++ backend/main.py
@@ -76,4 +76,4 @@ async def ping_db():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
